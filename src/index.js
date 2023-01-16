598239284a59991f13cbb3dfb9f5d0970707872c
uvorbs <yuval@orbs.com>
2023-01-16 14:51:51 +0200

    root is status

--- src/index.js
+++ src/index.js
@@ -9,13 +9,9 @@ const port = process.env.PORT || 8080;
 app.set('views', './views');
 app.set("view engine", "jade")
 
-app.get('/', function (req, res) {
-    res.render('sample');
-});
-
 app.use(express.static('css'));
 
-app.get('/status', function (req, res) {
+app.get('/', function (req, res) {
     res.render('status', status.get());
 });
 
